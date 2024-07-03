08a89dd443ce0b85f32da56c374dc0861aff9bfd
faridunechka17 <fadlushaa@gmail.com>
2024-07-03 18:25:51 +0600

    dz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const RegistrationForm = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [regis, setRegis] = useState(false)
+    const [error, setError] = useState('')
 
-export default App;
+    const userRegis = () => {
+        if (!username || !password || !confirmPassword) {
+            setError('');
+            return alert('заполните все поля!')
+        }else if (password !== confirmPassword) {
+            setError('');
+            return alert('пароли должны совпадать')
+        }else if (regis) {
+            setError('');
+            return alert('такой пользователь уже существует')
+        }else {
+            setRegis(true);
+            setError('');
+            alert('успешно прошли регистрацию!');
+        }
+    };
+
+    return (
+        <div>
+            <div>
+                <input onChange={(e) => setUsername(e.target.value)} type="text" placeholder="имя пользователя" value={username}/>
+                <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="пароль" value={password}/>
+                <input onChange={(e) => setConfirmPassword(e.target.value)} type="password" placeholder="подтвердите пароль" value={confirmPassword}/>
+
+                <button onClick={userRegis}>регистрация</button>
+            </div>
+            {error && <p>{error}</p>}
+
+        </div>
+    );
+};
+
+export default RegistrationForm;
